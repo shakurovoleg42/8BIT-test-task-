e6424040e0f44fa352ef43a1327a837dda0bfb05
Oleg Shakurov <thorsonodin2003@gmail.com>
2023-11-07 20:18:27 +0500

    +fix navigation

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,15 @@
 import '../css/index.css';
+
+const links = document.querySelectorAll('.header a');
+
+links.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const href = link.getAttribute('href');
+
+    document.querySelector(href).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
